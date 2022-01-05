99594a21056806c1dae4705128b256fc7886da0d
SAMPAIO1748 <57705851+SAMPAIO1748@users.noreply.github.com>
2022-01-05 14:13:20 +0100

    post_show

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -28,4 +28,14 @@ class PostController extends AbstractController
 
         return $this->render('posts.html.twig', ['posts' => $posts]);
     }
+
+    /**
+     * @Route("post/{id}", name="post_show")
+     */
+    public function postShow($id, PostRepository $postRepository)
+    {
+        $post = $postRepository->find($id); // find() permet de récupérer un poste grâce à son id.
+
+        return $this->render('post.html.twig', ['post' => $post]);
+    }
 }
