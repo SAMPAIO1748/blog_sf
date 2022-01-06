f126f44aad88d8e78463ca7847d5cd83e63524e3
SAMPAIO1748 <57705851+SAMPAIO1748@users.noreply.github.com>
2022-01-06 17:04:31 +0100

    CRUD category

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
+use App\Form\CategoryType;
 use App\Repository\CategoryRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -37,5 +41,84 @@ class CategoryController extends AbstractController
         $category = $categoryRepository->find($id);
 
         return $this->render("category.html.twig", ['category' => $category]);
+    }
+
+    /**
+     * @Route("/update/category/{id}", name="update_category")
+     */
+    public function updateCategory(
+        $id,
+        CategoryRepository $categoryRepository,
+        Request $request,
+        EntityManagerInterface $entityManagerInterface
+    ) {
+
+        $category = $categoryRepository->find($id);
+
+        $categoryForm = $this->createForm(CategoryType::class, $category);
+
+        $categoryForm->handleRequest($request);
+
+        if ($categoryForm->isSubmitted() && $categoryForm->isValid()) {
+            $entityManagerInterface->persist($category);
+            $entityManagerInterface->flush();
+
+            $this->addFlash(
+                'notice',
+                'La catégorie est modifiée'
+            );
+
+            return $this->redirectToRoute("category_list");
+        }
+
+        return $this->render("categoryform.html.twig", ['categoryForm' => $categoryForm->createView()]);
+    }
+
+    /**
+     * @Route("create/category", name="create_category")
+     */
+    public function createCategory(Request $request, EntityManagerInterface $entityManagerInterface)
+    {
+
+        $category = new Category();
+
+        $categoryForm = $this->createForm(CategoryType::class, $category);
+
+        $categoryForm->handleRequest($request);
+
+        if ($categoryForm->isSubmitted() && $categoryForm->isValid()) {
+            $entityManagerInterface->persist($category);
+            $entityManagerInterface->flush();
+
+            $this->addFlash(
+                'notice',
+                'La catégorie est ajoutée'
+            );
+
+            return $this->redirectToRoute("category_list");
+        }
+
+        return $this->render("categoryform.html.twig", ['categoryForm' => $categoryForm->createView()]);
+    }
+
+    /**
+     * @Route("delete/category/{id}", name="delete_category")
+     */
+    public function deleteCategory(
+        $id,
+        CategoryRepository $categoryRepository,
+        EntityManagerInterface $entityManagerInterface
+    ) {
+        $category = $categoryRepository->find($id);
+
+        $entityManagerInterface->remove($category);
+        $entityManagerInterface->flush();
+
+        $this->addFlash(
+            'notice',
+            'La catégorie est supprimée'
+        );
+
+        return $this->redirectToRoute("category_list");
     }
 }
