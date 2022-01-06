09cf58a28f0d848d55ac80aaacf38f34dd2e30e9
SAMPAIO1748 <57705851+SAMPAIO1748@users.noreply.github.com>
2022-01-06 12:09:58 +0100

    PostType amélioré

--- src/Form/PostType.php
+++ src/Form/PostType.php
@@ -2,7 +2,10 @@
 
 namespace App\Form;
 
+use App\Entity\Category;
 use App\Entity\Post;
+use App\Entity\Tag;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -15,6 +18,14 @@ class PostType extends AbstractType
         $builder
             ->add('title')
             ->add('content')
+            ->add('tag', EntityType::class, [
+                'class' => Tag::class,
+                'choice_label' => 'name'
+            ])
+            ->add('category', EntityType::class, [
+                'class' => Category::class,
+                'choice_label' => 'name'
+            ])
             ->add('enregistrer', SubmitType::class);
     }
 
